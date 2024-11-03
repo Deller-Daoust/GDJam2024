bab1ff977fe81f5cdcd8a9069b29afdab6489ace
ellieInfinity <elliev0125@gmail.com>
2024-11-03 14:58:50 -0500

    bunch of stuff

--- Assets/Scripts/PacPlayer.cs
+++ Assets/Scripts/PacPlayer.cs
@@ -1,32 +1,61 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PacPlayer : MonoBehaviour
 {
     private PacMove _pacMove;
+    
+    public Tile tile;
+    public Board board;
+    public Vector3Int position;
+    public Tilemap tilemap;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _pacMove = GetComponent<PacMove>();
+        this.tilemap.SetTile(position, tile);
+        //_pacMove = GetComponent<PacMove>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Keypad4))
+        if (Input.GetKeyDown(KeyCode.Keypad4))
         {
-            _pacMove.SetDirection("left");
+            this.tilemap.SetTile(position, null);
+            Move(Vector2Int.left);
+            this.tilemap.SetTile(position, tile);
+            
+            //_pacMove.SetDirection("left");
         }
-        else if (Input.GetKey(KeyCode.Keypad6))
+        else if (Input.GetKeyDown(KeyCode.Keypad6))
         {
-            _pacMove.SetDirection("right");
+            this.tilemap.SetTile(position, null);
+            Move(Vector2Int.right);
+            this.tilemap.SetTile(position, tile);
+            //_pacMove.SetDirection("right");
         }
-        else if (Input.GetKey(KeyCode.Keypad8))
+        else if (Input.GetKeyDown(KeyCode.Keypad8))
         {
-            _pacMove.SetDirection("up");
+            this.tilemap.SetTile(position, null);
+            Move(Vector2Int.up);
+            this.tilemap.SetTile(position, tile);
+            //_pacMove.SetDirection("up");
         }
-        else if (Input.GetKey(KeyCode.Keypad2))
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
         {
-            _pacMove.SetDirection("down");
+            this.tilemap.SetTile(position, null);
+            Move(Vector2Int.down);
+            this.tilemap.SetTile(position, tile);
+            //_pacMove.SetDirection("down");
         }
+    }
+    
+    private void Move(Vector2Int translation) {
+        Vector3Int newPosition = this.position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+        
+        this.position = newPosition;
     }
 }
